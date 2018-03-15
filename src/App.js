3e28e1f6122b494aa113971bd9f6ffc026c9b80e
delabroj <delabroj@users.noreply.github.com>
2018-03-15 16:07:47 -0600

    -

--- src/App.js
+++ src/App.js
@@ -19,16 +19,12 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer);
 
 class App extends Component {
-  //
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
+  }
 
   render() {
-    return (
-      <div className="App">
-        <p>{store.getState()}</p>
-        <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>+</button>
-        <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>-</button>
-      </div>
-    );
+    return <div className="App">//</div>;
   }
 }
 
